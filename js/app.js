f9e79df919fc948e03f0a4393d307eb7f9d6b0b6
Umer Inayat <umerinayat.369@gmail.com>
2020-05-12 07:02:36 +0500

    Export csv fix

--- js/app.js
+++ js/app.js
@@ -284,7 +284,7 @@ $(function () {
     $quallRCheck = $('#quallRCheck');
     isquallRExport = false;
 
-    $quntRCheck = $('#quallRCheck');
+    $quntRCheck = $('#quntRCheck');
     isquntRExport = false;
 
     var exportCsvFiles = function (event) {
